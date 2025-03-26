345a95f3546e6925a2e3eba13304be63af65551b
Devmoises79 <devmoisesss@gmail.com>
2025-03-26 00:33:47 -0300

    ConversãotTipo

---
+++ Conversao.py
@@ -0,0 +1,10 @@
+print(int('1'), type(int('1'))) # Convertendo string para int e mostrando o tipo
+print(int('1') + 1) # Convertendo string para int e somando tudo
+
+
+print(int('1'), type(int('1'))) # Convertendo string para int e mostrando o tipo
+print(float('1') + 1) # Convertendo string para float e somando tudo
+print(bool('')) # Convertendo string para bool e mostrando o tipo (*false nesse caso)
+print(bool(' ')) # Convertendo string para bool e mostrando o tipo (*true nesse caso)
+
+
